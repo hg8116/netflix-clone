6ce6ad9f1cfb4102376da655d84814919e788323
hg8116 <2011.harshgupta@gmail.com>
2022-06-13 01:42:29 +0530

    project

---
+++ src/components/Row.jsx
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import Movie from './Movie'
+import {MdChevronLeft, MdChevronRight} from 'react-icons/md'
+
+const Row = ({title, fetchURL}) => {
+  const [movies, setMovies] = useState([])
+
+  useEffect(() => {
+    axios.get(fetchURL)
+     .then((response) => {
+      setMovies(response.data.results)
+     })
+  }, [fetchURL])
+
+  return (
+    <>
+    <h2 className='text-white font-bold md:text-xl p-4'>{title}</h2>
+    <div className='relative flex items-center group'>
+      <MdChevronLeft className='bg-white left-4 rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block' size={40}/>
+      <div id={'slider'} className='w-full h-full overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide relative'>
+        {movies.map((item, id) => (
+          <Movie key={id} item={item}/>
+      ))}
+      </div>
+      <MdChevronRight className='bg-white right-4 rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block' size={40}/>
+    </div>
+    </>
+  )
+}
+
+export default Row
